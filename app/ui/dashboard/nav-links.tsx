95c04e43643a0d1e827681ffb9d67ffb4fca928b
AnithaKetha <anithaanjaneyulu2@gmail.com>
2025-07-30 11:31:54 +0530

    made nav-links component  as the client component

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,26 +1,19 @@
+'use client';
+ 
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
-import Link from 'next/link'
+import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
-
-// Map of links to display in the side navigation.
-// Depending on the size of the application, this would be stored in a database.
-const links = [
-  { name: 'Home', href: '/dashboard', icon: HomeIcon },
-  {
-    name: 'Invoices',
-    href: '/dashboard/invoices',
-    icon: DocumentDuplicateIcon,
-  },
-  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
-];
-
+ 
+// ...
+ 
 export default function NavLinks() {
-  const pathName = usePathname()
+  const pathname = usePathname();
+ 
   return (
     <>
       {links.map((link) => {
@@ -29,10 +22,10 @@ export default function NavLinks() {
           <Link
             key={link.name}
             href={link.href}
-                     className={clsx(
+            className={clsx(
               'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
               {
-                'bg-sky-100 text-blue-600': pathName === link.href,
+                'bg-sky-100 text-blue-600': pathname === link.href,
               },
             )}
           >
